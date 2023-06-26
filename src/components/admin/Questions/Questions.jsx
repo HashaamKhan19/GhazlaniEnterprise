5dc4b485b770b8fc1736d95e4e2a9cf3b93f707f
hashaam <hashaamkhan4247@gmail.com>
2023-06-27 02:20:05 +0500

    added questions page

--- src/components/admin/Questions/Questions.jsx
+++ src/components/admin/Questions/Questions.jsx
@@ -1,7 +1,57 @@
+import { Button, Group, Select, Stack, Textarea } from "@mantine/core";
 import React from "react";
+import Colors from "../../../utils/Colors";
 
 const Questions = () => {
-  return <div>Questions</div>;
+  return (
+    <form>
+      <Stack spacing={"xs"} mt={"xs"}>
+        <Select
+          placeholder="Select account level"
+          label="Account Level"
+          defaultValue="1"
+          data={["1", "2"]}
+          styles={{
+            item: {
+              "&[data-selected]": {
+                "&, &:hover": {
+                  backgroundColor: Colors.secondary,
+                  color: Colors.white,
+                },
+              },
+            },
+            label: {
+              fontSize: "1.2rem",
+              fontWeight: 600,
+              color: Colors.white,
+            },
+          }}
+        />
+        <Textarea
+          placeholder="Enter question"
+          label="Question"
+          styles={{
+            label: {
+              fontSize: "1.2rem",
+              fontWeight: 600,
+              color: Colors.white,
+            },
+          }}
+          minRows={2}
+        />
+      </Stack>
+      <Group position="right" mt={"md"}>
+        <Button
+          style={{
+            backgroundColor: Colors.primary,
+            width: "100px",
+          }}
+        >
+          Add
+        </Button>
+      </Group>
+    </form>
+  );
 };
 
 export default Questions;
